899f1e151b9df4308bdd897e5d0155268d92c495
a19jimsa <a19jimsa@student.his.se>
2021-11-27 12:17:47 +0100

    Tried to understand what to do added someting

--- bin/out.js
+++ bin/out.js
@@ -20540,22 +20540,38 @@ var require_forecastRoute = __commonJS({
     var express2 = require_express2();
     var router = express2.Router();
     var forecasts = [
-      { name: "Arjeplog", fromtime: "2020-01-01 00:00:00", totime: "2020-01-01 06:00:00", periodno: "0", periodname: "night", auxdata: { "TUNIT": "celsius", "TVALUE": "6.4", "ALTUNIT": "fahrenheit", "ALTVALUE": "43.52", "NUMBER": "4", "WSYMB3NUMBER": "6", "NAME": "Cloudy", "RUNIT": "mm", "RVALUE": "0", "DEG": "22", "CODE": "NNE", "NAME": "North-northeast", "MPS": "0.4", "NAME": "Calm", "UNIT": "hPa", "VALUE": "837" } },
-      { name: "Arjeplog", fromtime: "2020-01-02 00:00:00", totime: "2020-01-02 06:00:00", periodno: "0", periodname: "Night", auxdata: { "TUNIT": "celsius", "TVALUE": "-8.2", "ALTUNIT": "fahrenheit", "ALTVALUE": "17.24", "NUMBER": "10", "WSYMB3NUMBER": "23", "FNAME": "Sleet", "RUNIT": "mm", "RVALUE": "1.2", "DEG": "257", "CODE": "SW", "NAME": "Southwest", "MPS": "14.4", "NAME": "Near Gale", "UNIT": "hPa", "VALUE": "1276" } },
-      { name: "Arjeplog", fromtime: "2020-01-03 00:00:00", totime: "2020-01-03 06:00:00", periodno: "0", periodname: "Night", auxdata: { "TUNIT": "celsius", "TVALUE": "-8.7", "ALTUNIT": "fahrenheit", "ALTVALUE": "16.34", "NUMBER": "11", "WSYMB3NUMBER": "25", "FNAME": "Light snow", "RUNIT": "mm", "RVALUE": "1.7", "DEG": "257", "CODE": "W", "NAME": "West", "MPS": "15.3", "NAME": "Near Gale", "UNIT": "hPa", "VALUE": "1267" } },
-      { name: "Grums", fromtime: "2020-01-03 00:00:00", totime: "2020-01-03 06:00:00", periodno: "0", periodname: "Night", auxdata: { "TUNIT": "celsius", "TVALUE": "-8.7", "ALTUNIT": "fahrenheit", "ALTVALUE": "16.34", "NUMBER": "11", "WSYMB3NUMBER": "25", "FNAME": "Light snow", "RUNIT": "mm", "RVALUE": "1.7", "DEG": "257", "CODE": "W", "NAME": "West", "MPS": "15.3", "NAME": "Near Gale", "UNIT": "hPa", "VALUE": "1267" } }
+      { name: "Arjeplog", fromtime: "2020-01-01 00:00:00", totime: "2020-01-01 06:00:00", periodno: "0", periodname: "night", code: "Am", auxdata: { "TUNIT": "celsius", "TVALUE": "6.4", "ALTUNIT": "fahrenheit", "ALTVALUE": "43.52", "NUMBER": "4", "WSYMB3NUMBER": "6", "NAME": "Cloudy", "RUNIT": "mm", "RVALUE": "0", "DEG": "22", "CODE": "NNE", "NAME": "North-northeast", "MPS": "0.4", "NAME": "Calm", "UNIT": "hPa", "VALUE": "837" } },
+      { name: "Arjeplog", fromtime: "2020-01-02 00:00:00", totime: "2020-01-01 06:00:00", periodno: "0", periodname: "Night", code: "Am", auxdata: { "TUNIT": "celsius", "TVALUE": "-8.2", "ALTUNIT": "fahrenheit", "ALTVALUE": "17.24", "NUMBER": "10", "WSYMB3NUMBER": "23", "FNAME": "Sleet", "RUNIT": "mm", "RVALUE": "1.2", "DEG": "257", "CODE": "SW", "NAME": "Southwest", "MPS": "14.4", "NAME": "Near Gale", "UNIT": "hPa", "VALUE": "1276" } },
+      { name: "Arjeplog", fromtime: "2020-01-03 00:00:00", totime: "2020-01-03 06:00:00", periodno: "0", periodname: "Night", code: "Am", auxdata: { "TUNIT": "celsius", "TVALUE": "-8.7", "ALTUNIT": "fahrenheit", "ALTVALUE": "16.34", "NUMBER": "11", "WSYMB3NUMBER": "25", "FNAME": "Light snow", "RUNIT": "mm", "RVALUE": "1.7", "DEG": "257", "CODE": "W", "NAME": "West", "MPS": "15.3", "NAME": "Near Gale", "UNIT": "hPa", "VALUE": "1267" } },
+      { name: "Grums", fromtime: "2020-01-01 00:00:00", totime: "2020-01-03 06:00:00", periodno: "0", periodname: "Night", code: "Am", auxdata: { "TUNIT": "celsius", "TVALUE": "-8.7", "ALTUNIT": "fahrenheit", "ALTVALUE": "16.34", "NUMBER": "11", "WSYMB3NUMBER": "25", "FNAME": "Light snow", "RUNIT": "mm", "RVALUE": "1.7", "DEG": "257", "CODE": "W", "NAME": "West", "MPS": "15.3", "NAME": "Near Gale", "UNIT": "hPa", "VALUE": "1267" } }
     ];
     router.get("/", function(req, res) {
       res.status(200).json(forecasts);
       console.log("H\xE4mtade ut v\xE4derprognoser!");
     });
-    router.get("/:name/:fromtime", function(req, res) {
-      const forecast = forecasts.find((forecast2) => forecast2.name == req.params.name && forecast2.fromtime.substring(0, 10) == req.params.fromtime);
-      if (forecast) {
+    router.get("/:city/:date", function(req, res, next) {
+      const cityanddate = forecasts.slice(-1).find((forecast) => forecast.name == req.params.city && forecast.fromtime.substring(0, 10) == req.params.date);
+      if (cityanddate) {
         res.type("application/json");
-        res.status(200).send(forecast);
+        res.status(200).send(cityanddate);
       } else {
-        res.status(404).json({ msg: "Hittade ingen kod!" });
+        console.log("hittade inget p\xE5 stad och datum");
+        next();
+      }
+    });
+    router.get("/:code/:date", function(req, res) {
+      const codeanddate = forecasts.filter((codeanddate2) => codeanddate2.fromtime.substring(0, 10) == req.params.date && codeanddate2.code == req.params.code);
+      console.log(req.params.date);
+      const date = forecasts.filter((forecast) => forecast.fromtime.substring(0, 10) == req.params.date);
+      console.log(date);
+      if (codeanddate.length > 0) {
+        res.type("application/json");
+        res.status(200).send(codeanddate);
+      } else if (date.length > 0) {
+        res.type("application/json");
+        res.status(200).send(date);
+      } else {
+        res.status(404).json({ msg: "Hittade inget!" });
       }
     });
     router.get("/:name", function(req, res) {
@@ -20572,19 +20588,6 @@ var require_forecastRoute = __commonJS({
         res.status(200).send(fromtime);
       } else {
         res.status(404).json({ msg: "Hittade ingen med det namnet" });
-      }
-    });
-    router.get("/:climatecode/:date", function(req, res) {
-      const climatecode = forecasts.find((climatecode2) => climatecode2.code == req.body.code);
-      const climateanddate = forecasts.find((date) => date.fromtime.substring(0, 10) == req.body.date && date.code == req.body.code);
-      if (climatecode) {
-        res.type("application/json");
-        res.status(200).send(climatecode);
-      } else if (climateanddate) {
-        res.type("application/json");
-        res.status(200).send(climateanddate);
-      } else {
-        res.status(404).json({ msg: "Hittade ingen v\xE4rde!" });
       }
     });
     module2.exports = router;
--- src/forecastRoute.js
+++ src/forecastRoute.js
@@ -3,10 +3,10 @@ const express = require("express");
 const router =  express.Router();
 
 const forecasts = [
-            {name: "Arjeplog", fromtime: "2020-01-01 00:00:00", totime: "2020-01-01 06:00:00", periodno: "0", periodname: "night", auxdata: {"TUNIT":"celsius","TVALUE":"6.4","ALTUNIT":"fahrenheit","ALTVALUE":"43.52","NUMBER":"4","WSYMB3NUMBER":"6","NAME":"Cloudy","RUNIT":"mm","RVALUE":"0","DEG":"22","CODE":"NNE","NAME":"North-northeast","MPS":"0.4","NAME":"Calm","UNIT":"hPa","VALUE":"837"}},
-            {name: "Arjeplog",fromtime:"2020-01-02 00:00:00",totime:"2020-01-02 06:00:00",periodno:"0",periodname:"Night",auxdata:{"TUNIT":"celsius","TVALUE":"-8.2","ALTUNIT":"fahrenheit","ALTVALUE":"17.24","NUMBER":"10","WSYMB3NUMBER":"23","FNAME":"Sleet","RUNIT":"mm","RVALUE":"1.2","DEG":"257","CODE":"SW","NAME":"Southwest","MPS":"14.4","NAME":"Near Gale","UNIT":"hPa","VALUE":"1276"}},
-            {name:"Arjeplog",fromtime:"2020-01-03 00:00:00",totime:"2020-01-03 06:00:00",periodno:"0",periodname:"Night",auxdata:{"TUNIT":"celsius","TVALUE":"-8.7","ALTUNIT":"fahrenheit","ALTVALUE":"16.34","NUMBER":"11","WSYMB3NUMBER":"25","FNAME":"Light snow","RUNIT":"mm","RVALUE":"1.7","DEG":"257","CODE":"W","NAME":"West","MPS":"15.3","NAME":"Near Gale","UNIT":"hPa","VALUE":"1267"}},
-            {name:"Grums",fromtime:"2020-01-03 00:00:00",totime:"2020-01-03 06:00:00",periodno:"0",periodname:"Night",auxdata:{"TUNIT":"celsius","TVALUE":"-8.7","ALTUNIT":"fahrenheit","ALTVALUE":"16.34","NUMBER":"11","WSYMB3NUMBER":"25","FNAME":"Light snow","RUNIT":"mm","RVALUE":"1.7","DEG":"257","CODE":"W","NAME":"West","MPS":"15.3","NAME":"Near Gale","UNIT":"hPa","VALUE":"1267"}}
+            {name: "Arjeplog", fromtime: "2020-01-01 00:00:00", totime: "2020-01-01 06:00:00", periodno: "0", periodname: "night", code: "Am",auxdata: {"TUNIT":"celsius","TVALUE":"6.4","ALTUNIT":"fahrenheit","ALTVALUE":"43.52","NUMBER":"4","WSYMB3NUMBER":"6","NAME":"Cloudy","RUNIT":"mm","RVALUE":"0","DEG":"22","CODE":"NNE","NAME":"North-northeast","MPS":"0.4","NAME":"Calm","UNIT":"hPa","VALUE":"837"}},
+            {name: "Arjeplog",fromtime:"2020-01-02 00:00:00",totime:"2020-01-01 06:00:00",periodno:"0",periodname:"Night", code: "Am", auxdata:{"TUNIT":"celsius","TVALUE":"-8.2","ALTUNIT":"fahrenheit","ALTVALUE":"17.24","NUMBER":"10","WSYMB3NUMBER":"23","FNAME":"Sleet","RUNIT":"mm","RVALUE":"1.2","DEG":"257","CODE":"SW","NAME":"Southwest","MPS":"14.4","NAME":"Near Gale","UNIT":"hPa","VALUE":"1276"}},
+            {name:"Arjeplog",fromtime:"2020-01-03 00:00:00",totime:"2020-01-03 06:00:00",periodno:"0",periodname:"Night", code: "Am", auxdata:{"TUNIT":"celsius","TVALUE":"-8.7","ALTUNIT":"fahrenheit","ALTVALUE":"16.34","NUMBER":"11","WSYMB3NUMBER":"25","FNAME":"Light snow","RUNIT":"mm","RVALUE":"1.7","DEG":"257","CODE":"W","NAME":"West","MPS":"15.3","NAME":"Near Gale","UNIT":"hPa","VALUE":"1267"}},
+            {name:"Grums",fromtime:"2020-01-01 00:00:00",totime:"2020-01-03 06:00:00",periodno:"0",periodname:"Night", code: "Am",auxdata:{"TUNIT":"celsius","TVALUE":"-8.7","ALTUNIT":"fahrenheit","ALTVALUE":"16.34","NUMBER":"11","WSYMB3NUMBER":"25","FNAME":"Light snow","RUNIT":"mm","RVALUE":"1.7","DEG":"257","CODE":"W","NAME":"West","MPS":"15.3","NAME":"Near Gale","UNIT":"hPa","VALUE":"1267"}}
         ]
 
 router.get("/", function(req, res){
@@ -14,16 +14,36 @@ router.get("/", function(req, res){
     console.log("Hämtade ut väderprognoser!");
 })
 
-router.get("/:name/:fromtime", function(req, res){
-    const forecast = forecasts.find(forecast=>forecast.name==req.params.name&&forecast.fromtime.substring(0, 10)==req.params.fromtime);
-    if(forecast){
+// GET latest forecast from date and city. G
+router.get("/:city/:date", function(req, res, next){
+    const cityanddate = forecasts.slice(-1).find(forecast=>forecast.name==req.params.city&&forecast.fromtime.substring(0, 10)==req.params.date);
+    if(cityanddate){
         res.type("application/json");
-        res.status(200).send(forecast);
+        res.status(200).send(cityanddate);
     }else{
-        res.status(404).json({msg: "Hittade ingen kod!"});
+        console.log("hittade inget på stad och datum");
+        next();
     }
 })
-//Get forcecast from specific city or specific date
+
+//GET All forecast with code and date or date. VG
+router.get("/:code/:date", function(req, res){
+    const codeanddate = forecasts.filter(codeanddate=>codeanddate.fromtime.substring(0, 10) == req.params.date&&codeanddate.code==req.params.code);
+    console.log(req.params.date);
+    const date = forecasts.filter(forecast=>forecast.fromtime.substring(0,10) == req.params.date);
+    console.log(date);
+    if(codeanddate.length > 0){
+        res.type("application/json");
+        res.status(200).send(codeanddate);
+    }else if(date.length > 0){
+        res.type("application/json");
+        res.status(200).send(date);
+    }else{
+        res.status(404).json({msg:"Hittade inget!"});
+    }
+})
+
+//Get last forcecast from specific city or specific date
 router.get("/:name", function(req, res){
     console.log("Specifik stad: " + req.params.name);
     const city = forecasts.find(city=>city.name==req.params.name);
@@ -38,21 +58,6 @@ router.get("/:name", function(req, res){
         res.status(200).send(fromtime);
     }else{
        res.status(404).json({msg: "Hittade ingen med det namnet"});
-    }
-})
-
-//GET Get all cities with climatecode or climatecode and date
-router.get("/:climatecode/:date", function(req, res){
-    const climatecode = forecasts.find(climatecode=>climatecode.code == req.body.code);
-    const climateanddate = forecasts.find(date=>date.fromtime.substring(0,10)==req.body.date&&date.code == req.body.code);
-    if(climatecode){
-        res.type("application/json");
-        res.status(200).send(climatecode);
-    }else if(climateanddate){
-        res.type("application/json");
-        res.status(200).send(climateanddate);
-    }else{
-        res.status(404).json({msg: "Hittade ingen värde!"});
     }
 })
 
